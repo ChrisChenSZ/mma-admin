b64036be3cc9c43c035728685c5caa92faf1ffca
Chris <88316842@qq.com>
2018-01-18 22:05:05 +0800

    # default page

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div id="my"></div>
+    <div @click="change">点击</div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import $ from 'jquery'
+export default {
+  name: 'App',
+  data () {
+    return {
+      arr: [1, 2, 3, 4]
+    }
+  },
+  methods: {
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
